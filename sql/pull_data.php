c01416793670d29e63fc3a462a134ea9ce5519c7
agus24 <agusx244@gmail.com>
2017-03-12 22:37:20 +0700

    a

--- sql/pull_data.php
+++ sql/pull_data.php
@@ -1,5 +1,40 @@
 <?php
 require "../library/pull_data.class.php";
+require "../library/simple_html_dom.php";
+require "../library/leech_data.php";
+require "../library/leech_link.php";
 
-$pullData = new PullData("UNBN","SD",1,"http://tryoutunonline.com/soal-86-prediksi_soal_usbn.html");
-$pullData->setToDB();
+$link = leechLink();
+$pullData = new PullData;
+$query = new Query;
+
+$counter = 1;
+$data = array();
+// print_r(leechData("http://tryoutunonline.com/soal-41-soal_usbn.html"));
+for($i = 0 ; $i < count($link); $i++)
+{
+    echo $link[$i]['url'];
+    $hsl = leechData($link[$i]['url']);
+
+    if($hsl != "kosong"){
+        $data[] = $hsl;
+        echo "\n";
+    }
+    else{
+        $query->insert('link_error',"link='".$link[$i]['url']."'");
+        echo "\nkosong\n";
+    }
+}
+
+print_r($data);
+for($i = 0 ; $i < count($data); $i++)
+{
+    $matpel = $pullData->checkMatpel($link[$i]['bidstudi']);
+    $pullData->setMatpel($matpel);
+    $pullData->setData($data[$i]);
+    $pullData->setJenjang($link[$i]['kelas']);
+    $pullData->setTipeSoal($link[$i]['judul']);
+    $pullData->setToDB();
+}
+
+// print_r($pullData->getData());
